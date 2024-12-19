32922a71ae73f6c2504904f3fed9be98c714eca2
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2024-12-19 18:32:22 -0300

    tr

--- src/App.java
+++ src/App.java
@@ -1,11 +1,8 @@
-import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
-        System.out.println("olhar");
-        Scanner input = new Scanner(System.in);
+        
+        System.out.println("caixa eletronica");
 
     }
 }
