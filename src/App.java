b29db4f49151bdb504c9e0317f2c39b1cf1a27da
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2024-12-20 12:49:33 -0300

    em construção

--- src/App.java
+++ src/App.java
@@ -1,15 +1,76 @@
 import java.util.Scanner;
 
 public class App {
+	
+	public static void dinheiro(Scanner input) {
+		
+		
+		
+	}
+	public static void debito(Scanner input) {
+		
+		
+		
+	}
+	public static void credito(Scanner input) {
+		
+		
+		
+	}
+	
+
     public static void main(String[] args) throws Exception {
         
         System.out.println("caixa eletronica");
         
+        System.out.println("qua o tipo de pagamento.");
+        
         Scanner input = new Scanner(System.in);
         
-        int oi = input.nextInt(); 
+        while (true) {
+        	
+        	try {
+    			
+            	int pagamento = input.nextInt();
+            	
+            	switch (pagamento) {
+            	
+            		case 1:
+            			
+            			System.out.println("você escolheu em paga em dinheiro.");
+            			
+            			break;
+            			
+            		case 2:
+            			System.out.println("você escolheu em paga em débito.");
+            			
+            			break;
+            			
+            		case 3:
+            			System.out.println("você escolheu em paga em credito.");
+            			
+            			break;
+            		default:
+            			System.out.println("ops! essa opção não existe.");
+						break;
+    			}
+            	
+            	
+    		} catch (Exception e) {
+    			// TODO: handle exception
+    		}
+        	
+        	
+        	
+        }
         
-        System.out.println(oi);
-
-    }
+        
+			
+		}
+        
+        
+           
 }
+
+
+
