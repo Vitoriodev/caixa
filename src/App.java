0c20b0eda6f4782a04741e6498d35d80cc8f836c
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2024-12-19 22:51:15 -0300

    teste

--- src/App.java
+++ src/App.java
@@ -1,8 +1,15 @@
+import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) throws Exception {
         
         System.out.println("caixa eletronica");
+        
+        Scanner input = new Scanner(System.in);
+        
+        int oi = input.nextInt(); 
+        
+        System.out.println(oi);
 
     }
 }
