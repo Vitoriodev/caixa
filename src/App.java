986382daaef30fa4ec371a7ae6bba655f3c74018
Vitorio_Augusto <160076193+Vitoriodev@users.noreply.github.com>
2024-12-20 23:49:11 -0300

    1.0

--- src/App.java
+++ src/App.java
@@ -11,13 +11,15 @@ public class App {
 			System.out.print("qual foi o valor pago que foi passado");
 			float pago = input.nextFloat();
 				
-			float total = pago - valor;
+			float total = pago - valor; // vai fazer uma diminuição para saber quanto de troco vai passar por cliente
 						
 			if( pago > valor) {
 				
 				System.out.println("o seu troco foi "+total+" reais");
 				
-			}else if (pago < valor) {
+			}else if (pago < valor) { 
+				// quanto o valor do dinheiro for menor da compra vai dizer que é pouco dinheiro.
+				// e vai retorna ao inicio do valor da compra.
 				System.out.println("esta faltando "+total+" para fechar a conta.");
 				continue;				
 				
@@ -28,19 +30,35 @@ public class App {
 			break;
 		}
 		
-		
-		
-		
+					
 	}
 	public static void debito(Scanner input) {
 		
+		System.out.println("qual o valor da compra: ");
+		
+		// em construção.
 		
 		
 	}
 	public static void credito(Scanner input) {
 		
+		System.out.print("qual o valor da compra: ");
+		float valor = input.nextFloat();
 		
+		System.out.println("o valor da compra foi "+valor+" quantas vezes vai passa no cartão");
+		float credito = input.nextFloat();
 		
+		float pacelas = valor / credito;
+		
+				
+		if(credito >= 2) {
+			System.out.println("você pacelou "+credito+" vezes e vai pagar "+pacelas+" por pacelas.");
+			
+		}else {
+			System.out.println("você pacelou "+credito+" vez e vai pagar "+valor+" na proxima pacela");
+			
+		}
+	
 	}
 	
 
@@ -61,7 +79,6 @@ public class App {
             	switch (pagamento) {
             	
             		case 1:
-            			
             			System.out.println("você escolheu em paga em dinheiro.");            			
             			dinheiro(input);            			
             			break;
@@ -73,8 +90,9 @@ public class App {
             			
             		case 3:
             			System.out.println("você escolheu em paga em credito.");
-            			
+            			credito(input);
             			break;
+            			
             		default:
             			System.out.println("ops! essa opção não existe.");
 						break;
@@ -96,6 +114,3 @@ public class App {
         
            
 }
-
-
-
